fd235b6f4d9822a7e7b1730906f3941edd351a2e
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2023-11-10 17:05:24 +0000

    note making starts!!

---
+++ basics/variables-data_types.js
@@ -0,0 +1,14 @@
+console.log("Hello ji!!");
+// --> Variables and DataTypes in JavaScript!!
+const name = "Diwanshu";
+//const are immutable!!
+
+var umar = 19.1; 
+// var are not used currently
+let age = 20;
+let isLogged = false;
+// So we use let as they are mutable as well!
+
+/* There are different types of data types as well and some of them are:
+    boolean,undefined,string,numbr,etc.*/
+console.log(typeof umar);
