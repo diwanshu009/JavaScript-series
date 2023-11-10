84ebb61ee3bfba91a1faadbbc7a5c61d073c4466
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2023-11-10 17:09:02 +0000

    lets go!

--- basics/variables-data_types.js
+++ basics/variables-data_types.js
@@ -10,5 +10,7 @@ let isLogged = false;
 // So we use let as they are mutable as well!
 
 /* There are different types of data types as well and some of them are:
-    boolean,undefined,string,numbr,etc.*/
+               boolean,undefined,string,numbr,etc.            */
+
 console.log(typeof umar);
+//This is used to check type of data
