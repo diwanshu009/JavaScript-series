4ba390e4df6c30eb45f97c33cb958487442d757c
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2023-11-16 11:30:50 +0000

    loops

---
+++ core/loops.js
@@ -0,0 +1,18 @@
+// These are used to perform the similar task convienently
+
+for(let i = 1; i <= 10; i++) { // for loop
+    console.log(i)
+    // similar working of break and continue keyword as well!!
+}
+
+let i =1
+while(i<=10){ // while loop
+    console.log(i)
+    i++ 
+}
+
+let j =1
+do{          // do-while loop
+    console.log(j)
+    j++
+} while(j<=10)
