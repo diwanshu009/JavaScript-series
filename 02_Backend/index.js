2efc5a94d04b3d6135f264220c0b45c11638db5e
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2023-12-10 14:08:59 +0000

    Starting backend!!

---
+++ 02_Backend/index.js
@@ -0,0 +1,9 @@
+/* Node.js is a cross-platform, open-source JavaScript runtime environment that can run on Windows, Linux, Unix, macOS, 
+and more. Node.js runs on the V8 JavaScript engine, and executes JavaScript code outside a web browser. */
+
+// Node.js is used to create a http (client side) server (Hyper text transfer protocol)!!
+
+/* HTTP provides a standard between a web browser and a web server to establish communication. It is a set of rules 
+for transferring data from one computer to another. Data such as text, images, and other multimedia files are shared on 
+the World Wide Web. Whenever a web user opens their web browser, the user indirectly uses HTTP */
+
